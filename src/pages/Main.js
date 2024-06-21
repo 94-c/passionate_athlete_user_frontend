2885f9bb11bd20e371739a3b149a697a194d27e0
94-c <hyeongwoo26@gmail.com>
2024-06-21 20:58:15 +0900

    feat: footer 올림 기능 완료

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,14 @@
-// src/pages/Main1.js
 import React from 'react';
 import '../styles/Main.css';
 
-const Main1 = () => {
+const Main = () => {
   return (
     <div className="main-page">
       <div className="main-content">
-        <h1>Main Page 1</h1>
-        <p>Welcome to Main Page 1</p>
+        
       </div>
     </div>
   );
 };
 
-export default Main1;
+export default Main;

dc3a81fb76a92b8ff7b8ecc68cb5516c5d23d43d
94-c <hyeongwoo26@gmail.com>
2024-06-28 16:15:52 +0900

    refactor : 메인 화면 아이콘 변경

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import '../styles/Main.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDumbbell, faChartBar, faWeight, faClipboardList } from '@fortawesome/free-solid-svg-icons';
+import { faDumbbell, faChartBar, faWeight, faRunning } from '@fortawesome/free-solid-svg-icons';
 
 const Main = () => {
   return (
     <div className="main-page">
       <div className="button-container">
         <button className="button-item">
-          <FontAwesomeIcon icon={faClipboardList} className="button-icon" />
-          <span className="button-title">운동 기록</span>
+          <FontAwesomeIcon icon={faDumbbell} className="button-icon" />
+          <span className="button-title">운동</span>
         </button>
         <button className="button-item">
           <FontAwesomeIcon icon={faChartBar} className="button-icon" />
           <span className="button-title">통계</span>
         </button>
         <button className="button-item">
-          <FontAwesomeIcon icon={faDumbbell} className="button-icon" />
+          <FontAwesomeIcon icon={faRunning} className="button-icon" />
           <span className="button-title">오늘의 운동</span>
         </button>
         <button className="button-item">

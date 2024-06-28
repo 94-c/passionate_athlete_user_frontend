5d8102bc402f81cf00dd4b1a1ee1e2fca356a4cb
94-c <hyeongwoo26@gmail.com>
2024-06-28 20:39:52 +0900

    feat : 출석 달력 완료

--- src/pages/Attendance.js
+++ src/pages/Attendance.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import '../styles/Attendance.css';
+import { api } from '../api/Api.js';
+
 
 const Attendance = () => {
   const [date, setDate] = useState(new Date());
@@ -11,9 +13,13 @@ const Attendance = () => {
     const fetchAttendance = async () => {
       const year = date.getFullYear();
       const month = String(date.getMonth() + 1).padStart(2, '0');
-      const response = await fetch(`/attendances/monthly?month=${year}-${month}`);
-      const data = await response.json();
-      setPresentDays(data.presentDays.map(day => new Date(day)));
+      try {
+        const response = await api.get(`/attendances/monthly?month=${year}-${month}`);
+        const data = response.data;
+        setPresentDays(data.presentDays.map(day => new Date(day)));
+      } catch (error) {
+        console.error("Failed to fetch attendance data:", error);
+      }
     };
 
     fetchAttendance();
@@ -23,9 +29,22 @@ const Attendance = () => {
     setDate(newDate);
   };
 
+  const isSameDay = (date1, date2) => {
+    return date1.getDate() === date2.getDate() &&
+           date1.getMonth() === date2.getMonth() &&
+           date1.getFullYear() === date2.getFullYear();
+  };
+
   const tileClassName = ({ date, view }) => {
-    if (view === 'month' && presentDays.some(d => d.getTime() === date.getTime())) {
+    if (view === 'month' && presentDays.some(d => isSameDay(d, date))) {
       return 'present-day';
+    }
+    return null;
+  };
+
+  const tileContent = ({ date, view }) => {
+    if (view === 'month' && presentDays.some(d => isSameDay(d, date))) {
+      return <div className="highlight-dot"></div>;
     }
     return null;
   };
@@ -41,6 +60,7 @@ const Attendance = () => {
             formatDay={(locale, date) => date.toLocaleString('en', { day: 'numeric' })}
             locale="ko-KR"
             tileClassName={tileClassName}
+            tileContent={tileContent}
           />
         </div>
       </div>
@@ -49,3 +69,4 @@ const Attendance = () => {
 };
 
 export default Attendance;
+

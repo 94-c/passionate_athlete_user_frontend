09b80dadb8b93b88b945727cfb3716a1ebefa085
94-c <hyeongwoo26@gmail.com>
2024-07-02 17:38:02 +0900

    feat : 마이페이지 최근 인바디 정보 완료

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -27,9 +27,9 @@ const MyPage = () => {
     const isPositive = value > 0;
     const changeClass = isPositive ? 'positive-change' : 'negative-change';
     return (
-      <div className={`change ${changeClass}`}>
+      <span className={`change ${changeClass}`}>
         {isPositive ? `+${roundedValue}` : roundedValue}
-      </div>
+      </span>
     );
   };
 
@@ -45,42 +45,34 @@ const MyPage = () => {
             </div>
           </div>
         </div>
-        <h2 className="mypage-title">최근 인바디 정보</h2>
         {bodyInfo && (
-          <div className="mypage-body-info">
-            <div className="mypage-measure-date">
-              {new Date(bodyInfo.measureDate).toLocaleDateString()}
+          <>
+            <div className="mypage-body-info-header">
+              <h2 className="mypage-title">최근 인바디 정보</h2>
             </div>
-            <div className="mypage-body-info-row">
-              <div>체중</div>
-              <div>근육량</div>
-              <div>체지방량</div>
+            <div className="mypage-body-info">
+              <div className="mypage-body-info-row">
+                <div>체중</div>
+                <div>골격근량</div>
+                <div>체지방량</div>
+              </div>
+              <div className="mypage-body-info-row">
+                <div className="main-number-container">
+                  <span className="main-number">{bodyInfo.weight}</span>
+                  {formatChange(bodyInfo.weightChange)}
+                </div>
+                <div className="main-number-container">
+                  <span className="main-number">{bodyInfo.muscleMass}</span>
+                  {formatChange(bodyInfo.muscleMassChange)}
+                </div>
+                <div className="main-number-container">
+                  <span className="main-number">{bodyInfo.bodyFatMass}</span>
+                  {formatChange(bodyInfo.bodyFatMassChange)}
+                </div>
+              </div>
             </div>
-            <div className="mypage-body-info-row">
-              <div>
-                <span className="main-number">{bodyInfo.weight}</span>
-                {formatChange(bodyInfo.weightChange)}
-              </div>
-              <div>
-                <span className="main-number">{bodyInfo.muscleMass}</span>
-                {formatChange(bodyInfo.muscleMassChange)}
-              </div>
-              <div>
-                <span className="main-number">{bodyInfo.bodyFatMass}</span>
-                {formatChange(bodyInfo.bodyFatMassChange)}
-              </div>
-            </div>
-          </div>
+          </>
         )}
-        <h2 className="mypage-title">회원권 관리</h2>
-        <div className="mypage-membership-info">
-          <div className="mypage-membership-dates">
-            2024년 07월 02일 ~ 2024년 08월 01일
-          </div>
-          <div className="mypage-remaining-days">
-            현재 30일 남아있습니다.
-          </div>
-        </div>
       </div>
     </div>
   );

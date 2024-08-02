9a5f3eb373465487f35d655272aebb5f10ebb2b4
94-c <hyeongwoo26@gmail.com>
2024-08-02 17:15:27 +0900

    refactor : 운동 회원권 화면 수정 및 완료

--- src/pages/MemberShip.js
+++ src/pages/MemberShip.js
@@ -11,6 +11,7 @@ const MemberShip = () => {
   const [periodType, setPeriodType] = useState('ONE_MONTH');
   const [currentPage, setCurrentPage] = useState(1);
   const [showPauseHistoryModal, setShowPauseHistoryModal] = useState(false);
+  const [selectedHistoryId, setSelectedHistoryId] = useState(null);
   const itemsPerPage = 5;
 
   const fetchMembershipData = useCallback(async () => {
@@ -31,25 +32,21 @@ const MemberShip = () => {
     }
   }, []);
 
-  const fetchPauseHistory = useCallback(async () => {
-    if (membershipInfo) {
+  const fetchPauseHistory = useCallback(async (historyId) => {
+    if (historyId) {
       try {
-        const response = await api.get(`/memberships/${membershipInfo.id}/pause/history`);
+        const response = await api.get(`/memberships/${historyId}/pause/history`);
         setPauseHistory(response.data);
       } catch (error) {
         console.error('Error fetching pause history:', error);
       }
     }
-  }, [membershipInfo]);
+  }, []);
 
   useEffect(() => {
     fetchMembershipData();
     fetchMembershipHistory();
   }, [fetchMembershipData, fetchMembershipHistory]);
-
-  useEffect(() => {
-    fetchPauseHistory();
-  }, [membershipInfo, fetchPauseHistory]);
 
   const handlePause = async () => {
     try {
@@ -59,7 +56,7 @@ const MemberShip = () => {
       });
       alert('회원권이 정지되었습니다.');
       fetchMembershipData();
-      fetchPauseHistory();
+      fetchPauseHistory(selectedHistoryId);
     } catch (error) {
       console.error('Error pausing membership:', error);
       alert('회원권 정지에 실패했습니다.');
@@ -92,7 +89,9 @@ const MemberShip = () => {
     return Math.ceil(timeDiff / (1000 * 3600 * 24));
   };
 
-  const handleViewPauseHistoryClick = () => {
+  const handleViewPauseHistoryClick = (historyId) => {
+    setSelectedHistoryId(historyId);
+    fetchPauseHistory(historyId);
     setShowPauseHistoryModal(true);
   };
 
@@ -111,81 +110,82 @@ const MemberShip = () => {
         )}
       </div>
       {membershipInfo && (
-        <div className="membership-details">
-          <div className="membership-info">
-            <div>
-              <span>회원권 날짜 : {membershipInfo.startDate}</span> ~ <span>{membershipInfo.expiryDate}</span>
+        <div className="membership-container">
+          <div className="membership-details">
+            <div className="membership-info">
+              <div>
+                <span>회원권 날짜 : {membershipInfo.startDate}</span> ~ <span>{membershipInfo.expiryDate}</span>
+              </div>
+            </div>
+            <div className="membership-actions">
+              <div className="input-group">
+                <label>정지 일수:</label>
+                <input
+                  type="number"
+                  value={pauseDays}
+                  onChange={(e) => setPauseDays(e.target.value)}
+                  min="1"
+                  max="5"
+                />
+                <button onClick={handlePause}>정지</button>
+              </div>
+              <div className="input-group">
+                <label>갱신 기간:</label>
+                <select
+                  value={periodType}
+                  onChange={(e) => setPeriodType(e.target.value)}
+                >
+                  <option value="ONE_MONTH">1개월</option>
+                  <option value="THREE_MONTHS">3개월</option>
+                  <option value="ONE_YEAR">12개월</option>
+                </select>
+                <button onClick={handleRenew}>갱신</button>
+              </div>
             </div>
           </div>
-          <div className="membership-actions">
-            <div className="input-group">
-              <label>정지 일수:</label>
-              <input
-                type="number"
-                value={pauseDays}
-                onChange={(e) => setPauseDays(e.target.value)}
-                min="1"
-                max="5"
-              />
-              <button onClick={handlePause}>정지</button>
-            </div>
-            <div className="input-group">
-              <label>갱신 기간:</label>
-              <select
-                value={periodType}
-                onChange={(e) => setPeriodType(e.target.value)}
-              >
-                <option value="ONE_MONTH">1개월</option>
-                <option value="THREE_MONTHS">3개월</option>
-                <option value="ONE_YEAR">12개월</option>
-              </select>
-              <button onClick={handleRenew}>갱신</button>
+          <div className="membership-history-table-container">
+            {currentItems.length > 0 ? (
+              <>
+                <table className="membership-history-table">
+                  <thead>
+                    <tr>
+                      <th>갱신 기간</th>
+                      <th>이전 만료 날짜</th>
+                      <th>새 만료 날짜</th>
+                      <th>히스토리</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {currentItems.map((item) => (
+                      <tr key={item.id}>
+                        <td>{item.createdAt.split('T')[0]}</td>
+                        <td>{item.oldExpiryDate}</td>
+                        <td>{item.newExpiryDate}</td>
+                        <td>
+                          <button className="view-button" onClick={() => handleViewPauseHistoryClick(item.id)}>보기</button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </>
+            ) : (
+              <div className="no-history">히스토리가 없습니다.</div>
+            )}
+            <div className="pagination">
+              {Array.from({ length: Math.ceil(history.length / itemsPerPage) }, (_, index) => (
+                <button
+                  key={index + 1}
+                  onClick={() => handlePageChange(index + 1)}
+                  className={currentPage === index + 1 ? 'active' : ''}
+                >
+                  {index + 1}
+                </button>
+              ))}
             </div>
           </div>
         </div>
       )}
-      <h2 className="membership-title-left">회원권 히스토리</h2>
-      <div className="membership-history">
-        {currentItems.length > 0 ? (
-          <>
-            <table className="membership-history-table">
-              <thead>
-                <tr>
-                  <th>갱신 기간</th>
-                  <th>이전 만료 날짜</th>
-                  <th>새 만료 날짜</th>
-                  <th onClick={handleViewPauseHistoryClick} style={{ cursor: 'pointer', textDecoration: 'underline' }}>히스토리</th>
-                </tr>
-              </thead>
-              <tbody>
-                {currentItems.map((item) => (
-                  <tr key={item.id}>
-                    <td>{item.createdAt.split('T')[0]}</td>
-                    <td>{item.oldExpiryDate}</td>
-                    <td>{item.newExpiryDate}</td>
-                    <td>
-                      <button className="view-button" onClick={handleViewPauseHistoryClick}>보기</button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </>
-        ) : (
-          <div className="no-history">히스토리가 없습니다.</div>
-        )}
-        <div className="pagination">
-          {Array.from({ length: Math.ceil(history.length / itemsPerPage) }, (_, index) => (
-            <button
-              key={index + 1}
-              onClick={() => handlePageChange(index + 1)}
-              className={currentPage === index + 1 ? 'active' : ''}
-            >
-              {index + 1}
-            </button>
-          ))}
-        </div>
-      </div>
       {showPauseHistoryModal && (
         <MemberShipPauseHistoryModal
           isOpen={showPauseHistoryModal}

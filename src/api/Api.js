bc4322d66139871b14cbeca7727be077aa56db11
94-c <hyeongwoo26@gmail.com>
2024-08-14 13:53:05 +0900

    refactor : Api.js URL 변경

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL
+  baseURL: "http://58.79.16.12:9081/api/v1",
 });
 
 api.interceptors.request.use(
